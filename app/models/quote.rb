5ddf083a5526a3004eedb872f6127226c76a2fdc
Christopher Plain <christopher@jetcitycoder.com>
2017-01-10 21:11:26 +0000

    Added validation for user entered quotes

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,4 @@
 class Quote < ActiveRecord::Base
+  validates :quotation, presence: true, length: { maximum: 140, minimum: 3 }
+  validates :quotee, presence: true, length: { maximum: 50, minimum: 3 }
 end
